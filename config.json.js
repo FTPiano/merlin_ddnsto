86fdd9a4682d4b94a287ae149b511f896cdc86bd
Victor Xie <FTPiano@users.noreply.github.com>
2021-01-23 23:30:53 +0800

    Repack 2.9.3

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "koolshare小宝开发的基于http2的远程控制，仅支持远程管理路由器+nas+windows远程桌面。", 
     "home_url": "Module_ddnsto.asp", 
-    "md5": "64947cb087dd4607a2f5026612f6cd97", 
+    "md5": "d78bcbfc23e719850130c0ed9f2182e8", 
     "module": "ddnsto", 
     "title": "DDNSTO远程控制", 
     "version": "2.9.3"
